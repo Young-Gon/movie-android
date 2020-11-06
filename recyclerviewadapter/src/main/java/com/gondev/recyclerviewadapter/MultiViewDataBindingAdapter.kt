54ab58852776eca70881153b30b9e9d4760a8a8a
이영곤 <leeyg@brandi.co.kr>
2020-11-06 14:05:44 +0900

    grid type span count 추가

--- recyclerviewadapter/src/main/java/com/gondev/recyclerviewadapter/MultiViewDataBindingAdapter.kt
+++ recyclerviewadapter/src/main/java/com/gondev/recyclerviewadapter/MultiViewDataBindingAdapter.kt
@@ -5,28 +5,31 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.*
+import java.lang.IllegalArgumentException
 
 
 const val BINDING_VARIABLE_ID = -1
 
-class MultiViewDataBindingAdapter(
+open class MultiViewDataBindingAdapter(
     protected val lifecycleOwner: LifecycleOwner,
-    vararg param: Pair<Int, List<Pair<Int,Any>>>
-):ListAdapter<ItemType, RecyclerViewHolder<ItemType>>(object : DiffUtil.ItemCallback<ItemType>(){
-    override fun areItemsTheSame(oldItem: ItemType, newItem: ItemType)=
+    vararg param: Pair<Int, List<Pair<Int, Any>>>
+) : ListAdapter<ItemType, RecyclerViewHolder<ItemType>>(object : DiffUtil.ItemCallback<ItemType>() {
+    override fun areItemsTheSame(oldItem: ItemType, newItem: ItemType) =
         oldItem.layoutResId == newItem.layoutResId && oldItem.id == newItem.id
 
     override fun areContentsTheSame(oldItem: ItemType, newItem: ItemType) =
         oldItem == newItem
 }) {
 
-    private val layoutData= mapOf(*param)
+    private val layoutData = mapOf(*param)
 
     override fun getItemViewType(position: Int) = getItem(position).layoutResId
 
-    override fun onCreateViewHolder(parent: ViewGroup, layoutResId: Int): RecyclerViewHolder<ItemType> {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        layoutResId: Int
+    ): RecyclerViewHolder<ItemType> {
         var bindingVariableId = 0
         return RecyclerViewHolder(
             (DataBindingUtil.inflate(
@@ -36,7 +39,7 @@ class MultiViewDataBindingAdapter(
                 false
             ) as ViewDataBinding).apply {
                 layoutData[layoutResId]?.forEach {
-                    if(it.second== BINDING_VARIABLE_ID)
+                    if (it.second == BINDING_VARIABLE_ID)
                         bindingVariableId = it.first
                     else
                         setVariable(it.first, it.second)
@@ -46,12 +49,44 @@ class MultiViewDataBindingAdapter(
         )
     }
 
-    override fun onBindViewHolder(holder: RecyclerViewHolder<ItemType>, position: Int)  =
+    override fun onBindViewHolder(holder: RecyclerViewHolder<ItemType>, position: Int) =
         holder.onBindViewHolder(getItem(position))
 }
 
-interface ItemType{
+interface ItemType {
     val layoutResId: Int
     val id: Long
     override fun equals(other: Any?): Boolean
+}
+
+class GridDataBindingAdapter(
+    lifecycleOwner: LifecycleOwner,
+    val spanCount: Int,
+    vararg param: Pair<Int, List<Pair<Int, Any>>>
+) : MultiViewDataBindingAdapter(lifecycleOwner, *param) {
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+
+        val layoutManager = recyclerView.layoutManager
+        if (layoutManager == null || layoutManager !is GridLayoutManager)
+            throw IllegalArgumentException("GridLayoutManager가 필요 합니다")
+
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val item = getItem(position)
+                if (item is GridItemType)
+                    return item.spanSize
+
+                return 1
+            }
+        }
+    }
+}
+
+/**
+ * @see SpanSizeLookup
+ */
+interface GridItemType : ItemType {
+    val spanSize: Int
 }
